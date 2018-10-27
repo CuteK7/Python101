e090ab5c441b13eade4bc0ff87ab6665ae619506
Kevin <kevin.sjq@hotmail.com>
2018-10-27 14:11:16 +0800

    inputAndOutputChapter

---
+++ 03_TheFirstPythonProgram/inputAndOutput.py
@@ -0,0 +1,11 @@
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print(300)
+
+print(100+300)
+
+print('The answer of 100+200 is', 100+200)
+
+name = input('Hello, what\'s your name?\n')
+print('The name you input is', name)
+
